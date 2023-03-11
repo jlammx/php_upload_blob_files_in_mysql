555270cc163689456c9bf16b4ea421f8fcf8535f
Jorge Aguirre <jorge-l-am@hotmail.com>
2023-03-10 19:35:39 -0600

    Add the limit of files in the database

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
 <?php
 require_once __DIR__ . '/db.php';
 
-// Check if $_FILES count items
-if (count($_FILES) > 0) {
-    // Check if file was uploaded
-    if (is_uploaded_file($_FILES['userImage']['tmp_name'])) {
-        // Insert image as MySQL BLOB usign file_get_contents() to reads entire file into a string
-        $imgData = file_get_contents($_FILES['userImage']['tmp_name']);
-        $imgName = $_FILES['userImage']['name'];
-        $imgType = $_FILES['userImage']['type'];
-        $imgSize = $_FILES['userImage']['size'];
-        $imgTmpName = $_FILES['userImage']['tmp_name'];
-        $imgError = $_FILES['userImage']['error'];
-        $imgURL;
-        $sql = "INSERT INTO tbl_image(imageData, imageName, imageType, imageSize, imageTmpName, imageError, imageURL, upload_on) VALUES(?, ?, ?, ?, ?, ?, ?, NOW())";
-        $statement = $conn->prepare($sql);
-        $statement->bind_param('sssssss', $imgData, $imgName, $imgType, $imgSize, $imgTmpName, $imgError, $imgURL);
-        $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
-    } 
+// Get file count in table
+$sql = "SELECT COUNT(*) FROM tbl_image";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+$row = $result->fetch_row();
+// echo $row[0];
+
+// Validate the limit of files in the database
+if($row[0] < 15){
+    // Check if $_FILES count items
+    if (count($_FILES) > 0) {
+        // Check if file was uploaded
+        if (is_uploaded_file($_FILES['userImage']['tmp_name'])) {
+            // Insert image as MySQL BLOB usign file_get_contents() to reads entire file into a string
+            $imgData = file_get_contents($_FILES['userImage']['tmp_name']);
+            $imgName = $_FILES['userImage']['name'];
+            $imgType = $_FILES['userImage']['type'];
+            $imgSize = $_FILES['userImage']['size'];
+            $imgTmpName = $_FILES['userImage']['tmp_name'];
+            $imgError = $_FILES['userImage']['error'];
+            $imgURL;
+            $sql = "INSERT INTO tbl_image(imageData, imageName, imageType, imageSize, imageTmpName, imageError, imageURL, upload_on) VALUES(?, ?, ?, ?, ?, ?, ?, NOW())";
+            $statement = $conn->prepare($sql);
+            $statement->bind_param('sssssss', $imgData, $imgName, $imgType, $imgSize, $imgTmpName, $imgError, $imgURL);
+            $current_id = $statement->execute() or die("<b>Error:</b> Problem on Image Insert<br/>" . mysqli_connect_error());
+        } 
+    }
+}else{
+    echo "<div style='text-align: center; padding: 30px 0 10px 0; font-size: 20px; color: #c0392b'>
+    The allowed file limit has been reached <br/> Uploading more files is not allowed due to my condition code <br/> Thanks for coming!</div>";
 }
 ?>
 
